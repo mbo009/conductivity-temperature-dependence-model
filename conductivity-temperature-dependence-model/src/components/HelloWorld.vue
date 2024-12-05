d1b8748167b40f058bb281f9248ccce51fc9dac3
mciesla <miloszc009@gmail.com>
2024-12-05 21:51:09 +0100

    Added simple atom model

--- conductivity-temperature-dependence-model/src/components/HelloWorld.vue
+++ conductivity-temperature-dependence-model/src/components/HelloWorld.vue
@@ -21,37 +21,85 @@
         :max="atomSizeMax"
         label="Rozmiar atomów"
       />
+
+      <v-select
+        :items="materialNames"
+        v-model="selectedMaterial"
+        label="Wybierz materiał"
+        @change="updateTemperature"
+      ></v-select>
     </v-row>
     <p style="text-align: center">Prędkość dryfu:</p>
-    <PhysicsModel />
+    <PhysicsModel
+      :temperature="temperature"
+      :carrierConcentration="carrierConcentration"
+      :atomSize="atomSize"
+      :debeyeTemperature="debeyeTemperature"
+    />
   </v-container>
 </template>
 
 <script>
 import SliderWithField from "@/components/SliderWithField.vue";
+
 export default {
   components: {
     SliderWithField,
   },
+
   data() {
     return {
       temperature: 0,
       carrierConcentration: 1,
-      atomSize: 1,
+      atomSize: 30,
       temperatureMin: -273,
       temperatureMax: 2000,
       carrierConcentrationMin: 1,
       carrierConcentrationMax: 100,
-      atomSizeMin: 1,
-      atomSizeMax: 100,
+      atomSizeMin: 10,
+      atomSizeMax: 40,
+
+      materials: new Map([
+        ["Aluminium", 426],
+        ["Platyna", 240],
+        ["Kadm", 186],
+        ["Chrom", 610],
+        ["Srebro", 225],
+        ["Miedź", 344.5],
+        ["Złoto", 165],
+        ["Tytan", 420],
+        ["Żelazo", 464],
+        ["Wolfram", 405],
+        ["Nikiel", 440],
+        ["Cynk", 300],
+      ]),
+
+      selectedMaterial: "Aluminium",
     };
   },
+
+  computed: {
+    materialNames() {
+      return Array.from(this.materials.keys());
+    },
+    debeyeTemperature() {
+      return this.materials[this.selectedMaterial];
+    },
+  },
+
   methods: {
     validateInput(variable, min, max) {
       if (this[variable] < min) {
         this[variable] = min;
       } else if (this[variable] > max) {
         this[variable] = max;
+      }
+    },
+
+    updateTemperature(selectedMaterial) {
+      const temperature = this.materials.get(selectedMaterial);
+      if (temperature !== undefined) {
+        this.temperature = temperature;
       }
     },
   },
--- conductivity-temperature-dependence-model/src/components/PhysicsModel.vue
+++ conductivity-temperature-dependence-model/src/components/PhysicsModel.vue
@@ -6,36 +6,107 @@
 import Matter from "matter-js";
 
 export default {
+  props: {
+    atomSize: {
+      type: Number,
+      required: true,
+    },
+  },
   name: "PhysicsModel",
   mounted() {
-    let Engine = Matter.Engine,
-      Render = Matter.Render,
-      Runner = Matter.Runner,
-      Bodies = Matter.Bodies,
-      Composite = Matter.Composite;
+    this.initPhysics();
+  },
+  methods: {
+    initPhysics() {
+      this.engine = Matter.Engine.create({ gravity: { y: 0 } });
 
-    let engine = Engine.create();
+      let render = Matter.Render.create({
+        element: this.$refs.physicsModel,
+        engine: this.engine,
+        options: {
+          width: 1500,
+          height: 500,
+          wireframes: false,
+        },
+      });
+      this.addElectrodes();
+      this.addAtoms();
+      Matter.Render.run(render);
+      let runner = Matter.Runner.create();
+      Matter.Runner.run(runner, this.engine);
 
-    let render = Render.create({
-      element: this.$refs.physicsModel,
-      engine: engine,
-      options: {
-        width: 800,
-        height: 600,
-        wireframes: false,
+      setInterval(() => {
+        this.applyForces();
+      }, 16);
+    },
+
+    addElectrodes() {
+      let anode = Matter.Bodies.rectangle(20, 0, 40, 1000, {
+        label: "electrode",
+        render: {
+          text: "-",
+          fillStyle: "blue",
+        },
+      });
+      let cathode = Matter.Bodies.rectangle(1480, 0, 40, 1000, {
+        label: "electrode",
+        render: {
+          fillStyle: "red",
+        },
+      });
+      Matter.Composite.add(this.engine.world, [anode, cathode]);
+    },
+
+    addAtoms() {
+      this.atoms = [];
+      for (let i = 0; i < 50; i++) {
+        let atom = Matter.Bodies.circle(
+          100 + ((i % 10) * 1300) / 9,
+          50 + (Math.floor(i / 10) * 400) / 4,
+          this.atomSize,
+          {
+            restitution: 0.9,
+            friction: 0.01,
+            render: {
+              fillStyle: "green",
+            },
+          }
+        );
+        atom.ogPosition = { x: atom.position.x, y: atom.position.y };
+        this.atoms.push(atom);
+      }
+      Matter.Composite.add(this.engine.world, this.atoms);
+    },
+
+    updateAtoms() {
+      Matter.Composite.clear(this.engine.world, this.atoms);
+      this.addAtoms();
+      this.addElectrodes();
+    },
+
+    moveAtoms() {
+      //
+    },
+
+    moveCarriers() {
+      //
+    },
+
+    applyForces() {
+      this.moveAtoms();
+      this.moveCarriers();
+    },
+  },
+
+  watch: {
+    atomSize: {
+      immediate: true,
+      handler() {
+        if (this.engine) {
+          this.updateAtoms();
+        }
       },
-    });
-
-    let boxA = Bodies.rectangle(400, 200, 80, 80);
-    let boxB = Bodies.rectangle(450, 50, 80, 80);
-
-    let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-    Composite.add(engine.world, [boxA, boxB, ground]);
-
-    Render.run(render);
-    let runner = Runner.create();
-    Runner.run(runner, engine);
+    },
   },
 };
 </script>
